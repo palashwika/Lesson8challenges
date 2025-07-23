453200aac1f0dbf3cbf09685b70cedccdd7e1edf
palashwika <paladhiashwika@gmail.com>
2025-07-23 06:17:38 -0700

    Lesson8

---
+++ Operatorprecedence.py
@@ -0,0 +1,17 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y;
+print("Value of (v+w)* x/y is",z)
+
+
+name="John"
+age=2
+
+if name == "Alex" or name == "John" and age>=2: #or before and, considers alex and disregards john  
+    print("Hello! Welcome.")
+else:
+    print("Good Bye!!")
+
